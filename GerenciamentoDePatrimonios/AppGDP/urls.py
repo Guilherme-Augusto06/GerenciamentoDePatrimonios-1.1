ff5e21a0d1affc457ad165ce403cac2163a075eb
Guilherme-Augusto06 <ga375464@gmail.com>
2024-08-31 20:28:56 -0300

    feat: adicionando a rota para o profile

--- GerenciamentoDePatrimonios/AppGDP/urls.py
+++ GerenciamentoDePatrimonios/AppGDP/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
         path('homepageDark', views.homepageDark, name="homepageDark"),   # Inclui as urls do app blog
         path('login', views.login, name="login"),           # Inclui as urls do app blog
         path('cadastro', views.cadastro, name="cadastro"),   # Inclui as urls do app blog
+        path('profile', views.profile, name="profile"),       # Inclui as urls do app blog
 ]
